c78911ee5b86e344a307a96362c753a9e5119979
Vahan Sahakyan <vahan.sahakyan1996@gmail.com>
2023-04-17 11:04:02 +0400

    changing the asgmt jpa-query, adding resubmitted status option, adding the comment domain

--- src/main/java/io/github/vahansahakyan/CodeInspect/repository/AssignmentRepository.java
+++ src/main/java/io/github/vahansahakyan/CodeInspect/repository/AssignmentRepository.java
@@ -10,6 +10,8 @@ import java.util.Set;
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
   Set<Assignment> findByUser(User user);
 
-  @Query("select a from Assignment a where a.status = 'submitted'")
-  Set<Assignment> findByCodeReviewer(User user);
+  @Query("SELECT a FROM Assignment a "
+      + "WHERE (a.status = 'submitted'  AND (a.codeReviewer IS NULL OR a.codeReviewer = :codeReviewer))"
+      + "OR a.codeReviewer = :codeReviewer")
+  Set<Assignment> findByCodeReviewer(User codeReviewer);
 }
